1236021453db6c8d67f1e47a8720fe65b3a09cf7
Mario Mottl <mariomottl17@gmail.com>
2021-02-26 23:55:24 +0100

    Simple First Test of CPU
    More Commands/Instruction need to be added in the future.

---
+++ m6502.hpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+namespace m6502
+{
+    using Byte = unsigned char;
+    using Word = unsigned short;
+    using u32 = unsigned int;
+
+    class Memory
+    {
+    private:
+        static constexpr u32 MAX_MEM = 1024 * 64;
+        Byte Data[MAX_MEM];
+    public:
+        void Initialise();
+        Byte operator[](u32 Address) const;
+        Byte& operator[](u32 Address);
+    };
+
+    class CPU
+    {
+    private:
+        //Flags
+        Byte C : 1;	//0: Carry Flag	
+	    Byte Z : 1;	//1: Zero Flag
+	    Byte I : 1; //2: Interrupt disable
+	    Byte D : 1; //3: Decimal mode
+	    Byte B : 1; //4: Break
+	    Byte Unused : 1; //5: Unused
+	    Byte V : 1; //6: Overflow
+	    Byte N : 1; //7: Negative
+
+        Word PC;
+        Word SP;
+
+        //Registers
+        Byte A, X, Y;
+
+
+    public:
+        static constexpr Byte
+            INS_LDA_IM = 0x49;
+
+        void Reset(Memory &mem);
+        Byte FetchByte (u32 &Cycles, Memory &mem);
+        Byte ReadByte(u32 &Cycles, Byte Address, Memory &mem);
+        void Execute(u32 Cycles,Memory &mem);
+    };
+    
+}
+
+using namespace m6502;
---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include "m6502.hpp"  
+
+void Memory::Initialise()
+{
+    for (u32 i = 0; i <= MAX_MEM; i++)
+    {
+        Data[i] = 0;
+    }
+}
+
+Byte Memory::operator[](u32 Address) const
+{
+    return Data[Address];
+}
+
+Byte& Memory::operator[](u32 Address)
+{
+    return Data[Address];
+}
+
+void CPU::Reset(Memory &mem)
+{
+    PC = 0xFFFC;
+    SP = 0x0100;
+    C = Z = I = D= B = V = N = 0;
+    A = X = Y = 0;
+    mem.Initialise();
+}
+
+Byte CPU::FetchByte(u32 &Cycles, Memory &mem)
+{
+    Byte Data = mem[PC];
+    PC ++;
+    Cycles --;
+    return Data;
+}
+
+Byte CPU::ReadByte(u32 &Cycles, Byte Address, Memory &mem)
+{
+    Byte Data = mem[Address];
+    PC ++;
+    Cycles --;
+    return Data;    
+}
+
+void CPU::Execute(u32 Cycles, Memory &mem)
+{
+    while(Cycles > 0)
+    {
+        Byte Instruction = FetchByte(Cycles,mem);
+        switch (Instruction)
+        {
+            case INS_LDA_IM:
+            {
+                Byte Value = FetchByte(Cycles,mem);
+                A = Value;
+                Z = (A == 0);
+                N = (A & 0b10000000) > 0;
+            }
+            break;
+            default:
+            {
+                printf("No Instruction found");
+            }
+        }
+    }
+}
+
+int main()
+{
+    Memory mem;
+    CPU cpu;
+    cpu.Reset(mem);
+
+    //Test Memory
+    mem[0xFFFC] = CPU::INS_LDA_IM;
+    mem[0xFFFD] = 0x42;
+
+    cpu.Execute(2, mem);
+
+    return 0;
+}
